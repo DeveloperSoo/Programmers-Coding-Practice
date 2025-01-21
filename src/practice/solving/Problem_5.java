3e2fb5f120f498dfb95c3c4978dbb84f288b9cac
user1 <user1@DESKTOP-27PHPOM>
2025-01-21 18:43:41 +0900

    [ADD] 공배수
    [문제]
    정수 number와 n, m이 주어집니다. number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록
    solution 함수를 완성해주세요.
    
    [풀기전 예상]
    if문에서 %(모듈러스) 와 && 연산자로 시도

---
+++ src/practice/solving/Problem_5.java
@@ -0,0 +1,11 @@
+package practice.solving;
+
+public class Problem_5 {
+	 public int solution(int number, int n, int m) {
+	        if(number % n == 0 && number % m == 0 ){
+	            return 1;
+	        }else {            
+	        }return 0;
+	    }
+	
+}
