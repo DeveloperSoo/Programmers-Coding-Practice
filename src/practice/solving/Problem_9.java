39e48665eee536f8eac4fcc84e9cb27ce16b75d0
user1 <user1@DESKTOP-27PHPOM>
2025-01-22 16:00:52 +0900

    [ADD] 소문자로 바꾸기
    [풀기 전 예상]
    1. String answer에 toLowerCase() 메소드로 넣을 예정
    
    [결과]
    1차 성공

---
+++ src/practice/solving/Problem_9.java
@@ -0,0 +1,10 @@
+package practice.solving;
+
+public class Problem_9 {
+	class Solution {
+	    public String solution(String myString) {
+	        String answer = myString.toLowerCase();
+	        return answer;
+	    }
+	}
+}
