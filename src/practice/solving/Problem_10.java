ef568c9ec1b003c5878eabe5fce6fe1c2e17b138
user1 <user1@DESKTOP-27PHPOM>
2025-01-22 16:01:16 +0900

    [ADD] 대문자로 바꾸기
    [풀기 전 예상]
    1.소문자로 바꾸기랑 딱 반대로 하면 됨
    
    [결과]
    1차 성공

---
+++ src/practice/solving/Problem_10.java
@@ -0,0 +1,10 @@
+package practice.solving;
+
+public class Problem_10 {
+	class Solution {
+	    public String solution(String myString) {
+	        String answer = myString.toUpperCase();
+	        return answer;
+	    }
+	}
+}
