a917fb70fdf998a46a965e4e42bd571677191cf9
DeveloperSoo <developsooket@gmail.com>
2025-02-19 17:31:41 +0900

    [ADD] 문자열 곱하기
    [풀기 전 예상]
    1. 문자열 반복 메소드 사용해야지
    
    [결과]
    1차 성공

---
+++ src/practice/solving/Problem_16.java
@@ -0,0 +1,10 @@
+package practice.solving;
+
+public class Problem_16 {
+	class Solution {
+	    public String solution(String my_string, int k) {
+	        String answer = my_string.repeat(k);
+	        return answer;
+	    }
+	}	
+}
